2ba35f732afe43d2e4c755593b6a26ee9affcc77
Левченко Алексей Сергеевич <Levchenko.A.Sergeevic@sberbank.ru>
2020-12-17 23:02:58 +0300

    update flask app

--- load_vacancies.py
+++ load_vacancies.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     load_vacancies_pipeline()
     schedule = BlockingScheduler()
 
-    schedule.add_job(load_vacancies_pipeline, 'interval', minutes = 5)
+    schedule.add_job(load_vacancies_pipeline, 'interval', minutes = 60)
 
     schedule.add_job(health_check, 'interval', minutes = 1)
     schedule.start()
